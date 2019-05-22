3437d890868e7bc47af408af8875ab6e8f5fdcf2
justaduck98 <44838065+justaduck98@users.noreply.github.com>
2019-05-22 14:38:15 +0200

    added dynamic char array

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 
 
 int main(){
-
+int size = 10;
+char(*ary)[10] = new char[size][10];
 
 }
 
@@ -14,7 +15,7 @@ int main(){
 
 
 /*
-    -Input från en fil
+    -Input från en fil, dynamisk char/string array? alternativt vektor/er.
     -Kolla input och designera noder från filen
     -Länka noderna i ordningen som visas i filen, eg a -> b, alfa -> beta (verkar inte stå i samma nod-system)
     -Implementera algoritm för att kolla om det finns en hamiltoncykel tillgänglig

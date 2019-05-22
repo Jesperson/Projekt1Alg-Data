bfbab8675d1f65310c25f5f1e6448adc9920c4f6
Jesperson <jesperson@quickbrb.com>
2019-05-22 14:27:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,8 +14,15 @@ int main(){
 
 
 /*
+        -----Implementation 3-----
     -Input från en fil
     -Kolla input och designera noder från filen
     -Länka noderna i ordningen som visas i filen, eg a -> b, alfa -> beta (verkar inte stå i samma nod-system)
     -Implementera algoritm för att kolla om det finns en hamiltoncykel tillgänglig
+    -Start och slutpos avgörs av func
+*/
+
+/*
+    -
+
 */

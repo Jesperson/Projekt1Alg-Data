6a1f2d55f5cdf41253eb0852a302bd1f049ab739
Jesperson <jesperson@quickbrb.com>
2019-05-22 14:57:42 +0200

    .

--- main.cpp
+++ main.cpp
@@ -2,23 +2,35 @@
 #include <fstream>
 #include <string>
 
-
+int readFile(){}
 
 int main(){
+std::string fileName = argv[1];
 int contentSize = 10;
 char(*ary)[10] = new char[contentSize][10];
 
 }
 
+int readFile() {
+
+ std::ifstream myFile;
+ myFile.open(argv[1]);
+ char input[100];
+ if (myFile.is_open()) {
+ while (!myFile.eof()) {
+
+    myFile >> input;
+    std::cout << input;
+
+ }
+}
+myFile.close();
+return 0;
+}
 
 
 /*
-<<<<<<< HEAD
     -Input från en fil, dynamisk char/string array? alternativt vektor/er.
-=======
-        -----Implementation 3-----
-    -Input från en fil
->>>>>>> 9299172744b29c4352ff8ba389069932aa497b85
     -Kolla input och designera noder från filen
     -Länka noderna i ordningen som visas i filen, eg a -> b, alfa -> beta (verkar inte stå i samma nod-system)
     -Implementera algoritm för att kolla om det finns en hamiltoncykel tillgänglig

1207073679aaa0912200ccf240380de698378dea
Jesperson <jesperson@quickbrb.com>
2019-05-23 15:02:10 +0200

    Update readFile.cpp

--- readFile.cpp
+++ readFile.cpp
@@ -1,20 +1,23 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
 
-int readFile() {
+int readFile(argv[1])
+{
+   int argc, char *argv[];
 
- std::ifstream myFile;
- std::string fileName = argv[1];
- myFile.open(argv[1]);
- char input[100];
- if (myFile.is_open()) {
- while (!myFile.eof()) {
+   std::ifstream myFile;
+   std::string fileName = argv[1];
+   myFile.open(argv[1]);
+   char input[100];
+   if (myFile.is_open())
+   {
+      while (!myFile.eof())
+      {
 
-    myFile >> input;
-    std::cout << input;
-
- }
-}
-myFile.close();
-return 0;
+         myFile >> input;
+         std::cout << input;
+      }
+   }
+   myFile.close();
+   return 0;
 }

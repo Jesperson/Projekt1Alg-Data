d961547345a4b925629e515dec9c534ffaab3a53
justaduck98 <44838065+justaduck98@users.noreply.github.com>
2019-05-25 18:04:05 +0200

    posted latest code in readFile.cpp

--- readFile.cpp
+++ readFile.cpp
@@ -1,62 +1,274 @@
-#include <iostream>
-#include <fstream>
-#include <string>
+#include<iostream> 
+#include <list> 
 #include <vector>
+#include <fstream>
+  
+using namespace std; 
+  
+// This class represents a directed graph using 
+// adjacency list representation 
+class Graph 
+{ 
+    int n;    // No. of vertices 
+  
+    // Pointer to an array containing adjacency 
+    // lists 
+    list<int> *adj;    
+public: 
+    Graph(int n);  // Constructor 
+  
+    // function to add an edge to graph 
+    void addEdge(int v, int w);  
+  
+    // prints BFS traversal from a given source s 
+    void BFS(int s);   
+}; 
+  
+Graph::Graph(int n) 
+{ 
+    this->n = n; 
+    adj = new list<int>[n]; 
+} 
+  
+void Graph::addEdge(int v, int w) 
+{ 
+    adj[v].push_back(w); // Add w to v’s list. 
+} 
+  
+void Graph::BFS(int s) 
+{ 
+    // Mark all the vertices as not visited 
+    bool *visited = new bool[n]; 
+    for(int i = 0; i < n; i++) 
+        visited[i] = false; 
+  
+    // Create a queue for BFS 
+    list<int> queue; 
+  
+    // Mark the current node as visited and enqueue it 
+    visited[s] = true; 
+    queue.push_back(s); 
+  
+    // 'i' will be used to get all adjacent 
+    // vertices of a vertex 
+    list<int>::iterator i; 
+  
+    while(!queue.empty()) 
+    { 
+        // Dequeue a vertex from queue and print it 
+        s = queue.front(); 
+        cout << s << " "; 
+        queue.pop_front(); 
+  
+        // Get all adjacent vertices of the dequeued 
+        // vertex s. If a adjacent has not been visited,  
+        // then mark it visited and enqueue it 
+        for (i = adj[s].begin(); i != adj[s].end(); ++i) 
+        { 
+            if (!visited[*i]) 
+            { 
+                visited[*i] = true; 
+                queue.push_back(*i); 
+            } 
+        } 
+    } 
+} 
+  
+// Driver program to test methods of graph class 
+//******************************************************************************************************
+/*int main(int argc, char *argv[]) 
+{ 
+    ifstream myFile;
+	string fileName = argv[1];
+    vector<string> nodes;
+    vector<int> nodesInt;
+    bool emptyLineFound = false;
+    string line;
+    int n = -1;
 
-using namespace std;
+    myFile.open(argv[1]);
+    if (myFile.is_open()){
+        while (emptyLineFound == false)
+		{
+			getline(myFile, line);
+			if (line.size() == 0)
+			{
+				cout << "found empty line" << endl;
+				emptyLineFound = true;
+			}
+
+			else if (n == -1)
+			{
+				cout << "First line found" << endl;
+				n++;
+				continue;
+			}
+			else
+			{
+				nodes.push_back(line);
+				nodesInt.push_back(n);
+				cout << nodes[n] << endl;
+				n++;
+			}
+		}
+        Graph graph1(n);
+		cout << endl;
+		int columnInFile = 0; //stående, hjälper datorn hålla koll vilken del av filen den jobbar med, och hanterar informationen därefter.
+		int nodeX = 0;
+		int nodeY = 0;
+		while (myFile >> line)
+		{
+			if (columnInFile == 0)
+			{
+
+				nodeX = compareStringToVector(line, nodes, n);
+				cout << "Name of node is: " << line << ", ";
+				columnInFile = 1;
+			}
+			else if (columnInFile == 1)
+			{
+				nodeY = compareStringToVector(line, nodes, n);
+				cout << " and the node it should connect to is: " << line << ". " << endl;
+				columnInFile = 2;
+			}
+			else if (columnInFile == 2)
+			{
+				graph1.addEdge(nodeX, nodeY);
+				//cout << line << endl;
+				columnInFile = 0;
+				nodeX = 0;
+				nodeY = 0;
+			}
+		}
+    }
+    // Create a graph given in the above diagram 
+    Graph g(4); 
+    g.addEdge(0, 1); 
+    g.addEdge(0, 2); 
+    g.addEdge(1, 2); 
+    g.addEdge(2, 0); 
+    g.addEdge(2, 3); 
+    g.addEdge(3, 3); 
+  
+    cout << "Following is Breadth First Traversal "
+         << "(starting from vertex 2) \n"; 
+    g.BFS(2); 
+  
+    return 0; 
+} */
+
+// ******************************************************************************************************
+int compareStringToVector(string comparison, vector<string> vectorWithNodes, int n);
 int main(int argc, char *argv[])
 {
 
-   ifstream myFile;
-   string fileName = argv[1];
-   vector<string> nodes;
-   vector<vector<string>> connected;
-   string line;
-   int n = -1;
+	ifstream myFile;
+	string fileName = argv[1];
+	vector<string> nodes;
+	list<int> nodesInt;
+	//list<vector<string>> connected;
 
-   myFile.open(argv[1]);
-   if (myFile.is_open())
-   {
+	string line;
+	bool emptyLineFound = false;
+	int n = -1;
 
-      cout << "opened the file" << endl;
+	//test(nodes);
+	myFile.open(argv[1]);
+	if (myFile.is_open())
+	{
 
-      while (getline(myFile, line))
-      {
-         //myFile >> node[n];
+		cout << "opened the file" << endl;
 
-         //node[n] = line;
-         if (line.size() == 0)
-         {
-            cout << "found empty line, breaking." << endl;
-            break;
-         }
-         else if (n == -1)
-         {
-            cout << "First line found" << endl;
-            n++;
-            continue;
-         }
-         else
-         {
-            cout << line + " ";
-            nodes[n].push_back(line[0]);
-            cout << nodes[n][0] << endl;
-            n++;
-         }
-      };
+		while (emptyLineFound == false)
+		{
+			getline(myFile, line);
+			if (line.size() == 0)
+			{
+				cout << "found empty line" << endl;
+				emptyLineFound = true;
+			}
 
-      pointerFunction(nodeX, nodeY){
-         for(int n = 0; n <= nodes.size(); n++){
-            connected[nodeX][n] = nodes[nodeY];
-            *nodes[nodeX] -> nodes[nodeY];
-         }
+			else if (n == -1)
+			{
+				cout << "First line found" << endl;
+				n++;
+				continue;
+			}
+			else
+			{
+				nodes.push_back(line);
+				nodesInt.push_back(n);
+				cout << nodes[n] << endl;
+				n++;
+			}
+		}
+		//change input method to handle ex. Alpha'\t'Gamma'\t'2
+		Graph graph1(n);
+		cout << endl;
+		int columnInFile = 0; //stående, hjälper datorn hålla koll vilken del av filen den jobbar med, och hanterar informationen därefter.
+		int nodeX = 0;
+		int nodeY = 0;
+		while (myFile >> line)
+		{
+			if (columnInFile == 0)
+			{
 
-      }
-   }
-   else
-   {
-      cout << "File not found.";
-   }
-   myFile.close();
-   return 0;
+				nodeX = compareStringToVector(line, nodes, n);
+				cout << "Name of node is: " << line << ", ";
+				columnInFile = 1;
+			}
+			else if (columnInFile == 1)
+			{
+				nodeY = compareStringToVector(line, nodes, n);
+				cout << " and the node it should connect to is: " << line << ". " << endl;
+				columnInFile = 2;
+			}
+			else if (columnInFile == 2)
+			{
+				graph1.addEdge(nodeX, nodeY);
+				//cout << line << endl;
+				columnInFile = 0;
+				nodeX = 0;
+				nodeY = 0;
+			}
+		}
+        //fix this
+		graph1.BFS(4);
+	}
+	else
+	{
+		cout << "File not found.";
+	}
+
+	cout << "From vector: ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << nodes[i] << " ";
+	}
+	list<int>::iterator l;
+	cout << endl
+		 << "From list: ";
+	for (l = nodesInt.begin(); l != nodesInt.end(); l++)
+	{
+		cout << *l << " ";
+	}
+	myFile.close();
+	return 0;
+}
+
+int compareStringToVector(string comparison, vector<string> vectorWithNodes, int n)
+{
+	int caseNr = -1;
+	for (int y = 0; y < n; y++)
+	{
+		if (vectorWithNodes[y] == comparison)
+		{
+			caseNr = y;
+		}
+	}
+	if (caseNr == -1)
+	{
+		cout << "Something went wrong chief... sorry boot dat." << endl;
+	}
+	return caseNr;
 }

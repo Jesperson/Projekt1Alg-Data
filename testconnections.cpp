aeb6de48257f84dae595189c9ac841d168ed459e
justaduck98 <44838065+justaduck98@users.noreply.github.com>
2019-05-25 23:29:55 +0200

    pog

--- testconnections.cpp
+++ testconnections.cpp
@@ -1,253 +1,276 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <list>
 #include <vector>
+#include <fstream>
+#include <algorithm>
+#include <string.h>
 
 using namespace std;
 
-class representGraph
+// This class represents a directed graph using
+// adjacency list representation
+class Graph
 {
-private:
-	int n;
-    int e = 0;
-	list<int> *adjacencyMatrix;
-
+  int V;          // Antal kanter
+  list<int> *adj; // Lista för att hålla koll på länkar
+  int startingNode = 0;
 public:
-	representGraph(int n)
-	{
-		this->n = n;
-		adjacencyMatrix = new list<int>[n];
-	}
-	~representGraph() { delete[] adjacencyMatrix; }
+  // Const & destr
+  Graph(int V)
+  {
+    this->V = V;
+    adj = new list<int>[V];
+  }
+  ~Graph() { delete[] adj; }
+  string result = "2\n";
+  // Lägg till kant
+  void addEdge(int u, int v)
+  {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+  void rmvEdge(int u, int v);
 
-	void connectNodes(int x, int y);
-	void DFSFunction(int n, bool visited[]); //DFS was the easiest algorithm to implement a graph, and that's why we decided to use this.
-	bool checkAllConnections();
-	bool pathAvailable(); //add int list as parameter here <3
-	void outputResultToFile(string result, bool possible);
-	void test(representGraph &g);
+  // Skriv output till variabel (outputResult)
+  void printEulerTour(vector<string> nodes);
+  void printEulerUtil(int s, vector<string> nodes, string resultat);
+
+  // Räkna antal kanter som finns tillgängliga från en nod
+  int countEdge(int v, bool visited[]);
+
+  // Kolla om kanten funkar (om den inte redan är "använd" aka -1)
+  bool isValidNextEdge(int u, int v);
 };
 
-void representGraph::connectNodes(int x, int y)
+/* The main function that print Eulerian Trail. It first finds an odd 
+   degree vertex (if there is any) and then calls printEulerUtil() 
+   to print the path */
+//
+void Graph::printEulerTour(vector<string> nodes)
 {
-	cout << "Connecting node: " << x << " with node " << y << endl;
-	adjacencyMatrix[x].push_back(y); 
-	adjacencyMatrix[y].push_back(x);
-	e++;
+  // Hitta en kant med udda gradtal
+  string result;
+  for (int i = 0; i < V; i++){
+    if (adj[i].size() % 2 == 1)
+    {
+      startingNode = i;
+      cout << "Starting node is: " << startingNode << "and adj[i] is: " << adj[i].size() << endl;
+      break;
+    }
+  }
+    result = nodes[startingNode];
+  // Print tour starting from oddv
+  printEulerUtil(startingNode, nodes, result);
+  cout << endl;
 }
 
-void representGraph::DFSFunction(int nodeX, bool visited[]) //change this to know that a road was used, not a node.
+// Skriver (INTE GJORD ÄN) nodkoppling
+void Graph::printEulerUtil(int u, vector<string> nodes, string result)
 {
-	cout << "Made it into the DFSFunction, with nodeX value as: " << nodeX << endl;
-	//cout << "which road gets used: " << visited[nodeX] << endl; 
-	visited[nodeX] = true;
-	list<int>::iterator i;
-	for (i = adjacencyMatrix[nodeX].begin(); i != adjacencyMatrix[nodeX].end(); i++)
-	{
+  // Recur for all the vertices adjacent to this vertex
+  list<int>::iterator i;
+  for (i = adj[u].begin(); i != adj[u].end(); ++i)
+  {
+    int v = *i;
 
-		if (!visited[*i])
-		{
-
-			DFSFunction(*i, visited);
-		}
-	}
+    // If edge u-v is not removed and it's a a valid next edge
+    if (v != -1 && isValidNextEdge(u, v))
+    {
+      //cout << "Trying to output the result string after assigning (or adding) the noder[u] name to the string." << endl;
+      result = result + " " + " -> " + nodes[v];
+      //cout << result;
+      //cout << noder[1];
+      cout << result;
+      //cout << nodes[u] << "-" << nodes[v] << "  ";
+      rmvEdge(u, v);
+      printEulerUtil(v, nodes, result);
+    }
+  }
 }
-bool representGraph::checkAllConnections()
+
+// The function to check if edge u-v can be considered as next edge in
+// Euler Tout
+bool Graph::isValidNextEdge(int u, int v)
 {
-	cout << "Made it to the connection func. " << endl;
-	bool connected = true;
-	bool visited[e];
-	int i;
-	for (i = 0; i < e; i++)
-	{
-		visited[i] = false;
-	}
-	cout << e << endl;
-	/*for (i = 0; i < n; i++){
-		cout 
-	}*/
-	for (i = 0; i < n; i++)
-	{
-		if (adjacencyMatrix[i].size() != 0)
-		{
-			break;
-		}
-	}
-	if (i == n)
-	{
-		connected = true;
-	}
+  // The edge u-v is valid in one of the following two cases:
 
-	DFSFunction(i, visited);
+  // 1) If v is the only adjacent vertex of u
+  int count = 0; // To store count of adjacent vertices
+  list<int>::iterator i;
+  for (i = adj[u].begin(); i != adj[u].end(); ++i)
+    if (*i != -1)
+      count++;
+  if (count == 1)
+    return true;
 
-	for (i = 0; i < n; i++)
-	{
-		if (visited[i] == false && adjacencyMatrix[i].size() > 0)
-		{	
-			cout << "Node nr: " << i << " was not connected."<< endl;
-			connected = false;
-		}
-	}
-	cout << "result from connection func: " << connected << endl;
-	return connected;
+  // 2) If there are multiple adjacents, then u-v is not a bridge
+  // Do following steps to check if u-v is a bridge
+
+  // 2.a) count of vertices reachable from u
+  bool visited[V];
+  memset(visited, false, V);
+  int count1 = countEdge(u, visited);
+
+  // 2.b) Remove edge (u, v) and after removing the edge, count
+  // vertices reachable from u
+  rmvEdge(u, v);
+  memset(visited, false, V);
+  int count2 = countEdge(u, visited);
+
+  // 2.c) Add the edge back to the graph
+  addEdge(u, v);
+
+  // 2.d) If count1 is greater, then edge (u, v) is a bridge
+  return (count1 > count2) ? false : true;
 }
 
-bool representGraph::pathAvailable()
-{ //add list as parameter here <3
-
-	int possible = -1;
-	string result;
-	//checks if all non-isolated nodes are connected to eachother, aka not 2 graphs or similar problems
-	if (checkAllConnections() == false)
-	{
-		possible = 0;
-	}
-
-	//checks for the number of odd nodes, using the size of the list of nodes connected to a certain node
-	int odd = 0;
-	int oddNodes[n];
-	for (int i = 0; i < n; i++)
-	{
-		if (adjacencyMatrix[i].size() % 2 == 1)
-		{
-			oddNodes[odd] = i;
-			odd++;
-		}
-	}
-	//checks if the graph has more than 2 odd nodes, it is impossible to solve.
-	if (odd > 2)
-	{
-		possible = 0;
-		result = "2\nNO PATH FOUND";
-		outputResultToFile(result, possible);
-		cout << result;
-	}
-	return possible;
-}
-void representGraph::outputResultToFile(string result, bool possible)
+// This function removes edge u-v from graph.  It removes the edge by
+// replacing adjcent vertex value with -1.
+void Graph::rmvEdge(int u, int v)
 {
-	cout << "made it into the output." << endl;
-	ofstream outputFile;
-	outputFile.open("Output.txt");
-	outputFile << result;
-	outputFile.close();
+  // Find v in adjacency list of u and replace it with -1
+  list<int>::iterator iv = find(adj[u].begin(), adj[u].end(), v);
+  *iv = -1;
+
+  // Find u in adjacency list of v and replace it with -1
+  list<int>::iterator iu = find(adj[v].begin(), adj[v].end(), u);
+  *iu = -1;
 }
-void representGraph::test(representGraph &g)
+
+// A DFS based function to count reachable vertices from v
+int Graph::countEdge(int v, bool visited[])
 {
-	int res = g.pathAvailable();
+  // Mark the current node as visited
+  visited[v] = true;
+  int count = 1;
+
+  // Recur for all vertices adjacent to this vertex
+  list<int>::iterator i;
+  for (i = adj[v].begin(); i != adj[v].end(); ++i)
+    if (*i != -1 && !visited[*i])
+      count += countEdge(*i, visited);
+
+  return count;
 }
+
+void printToOutput(string result)
+{
+  ofstream output;
+  output.open("Output.txt");
+  output << result;
+  output.close();
+}
+
 int compareStringToVector(string comparison, vector<string> vectorWithNodes, int n);
 int main(int argc, char *argv[])
 {
+  vector<string> nodes;
+  ifstream myFile;
+  string fileName = argv[1];
+  list<int> nodesInt;
+  //list<vector<string>> connected;
 
-	ifstream myFile;
-	string fileName = argv[1];
-	vector<string> nodes;
-	list<int> nodesInt;
-	//list<vector<string>> connected;
+  string line;
+  bool emptyLineFound = false;
+  int n = -1;
 
-	string line;
-	bool emptyLineFound = false;
-	int n = -1;
+  //test(nodes);
+  myFile.open(argv[1]);
+  if (myFile.is_open())
+  {
 
-	//test(nodes);
-	myFile.open(argv[1]);
-	if (myFile.is_open())
-	{
+    cout << "opened the file" << endl;
 
-		cout << "opened the file" << endl;
+    while (emptyLineFound == false)
+    {
+      getline(myFile, line);
+      if (line.size() == 0)
+      {
+        cout << "found empty line" << endl;
+        emptyLineFound = true;
+      }
 
-		while (emptyLineFound == false)
-		{
-			getline(myFile, line);
-			if (line.size() == 0)
-			{
-				cout << "found empty line" << endl;
-				emptyLineFound = true;
-			}
+      else if (n == -1)
+      {
+        cout << "First line found" << endl;
+        n++;
+        continue;
+      }
+      else
+      {
+        nodes.push_back(line);
+        nodesInt.push_back(n);
+        cout << nodes[n] << endl;
+        n++;
+      }
+    }
+    //change input method to handle ex. Alpha'\t'Gamma'\t'2
+    Graph graph1(n);
+    cout << endl;
+    int columnInFile = 0; //stående, hjälper datorn hålla koll vilken del av filen den jobbar med, och hanterar informationen därefter.
+    int nodeX = 0;
+    int nodeY = 0;
+    while (myFile >> line)
+    {
+      if (columnInFile == 0)
+      {
 
-			else if (n == -1)
-			{
-				cout << "First line found" << endl;
-				n++;
-				continue;
-			}
-			else
-			{
-				nodes.push_back(line);
-				nodesInt.push_back(n);
-				cout << nodes[n] << endl;
-				n++;
-			}
-		}
-		//change input method to handle ex. Alpha'\t'Gamma'\t'2
-		representGraph graph1(n);
-		cout << endl;
-		int columnInFile = 0; //stående, hjälper datorn hålla koll vilken del av filen den jobbar med, och hanterar informationen därefter.
-		int nodeX = 0;
-		int nodeY = 0;
-		while (myFile >> line)
-		{
-			if (columnInFile == 0)
-			{
+        nodeX = compareStringToVector(line, nodes, n);
+        cout << "Name of node is: " << line << ", ";
+        columnInFile = 1;
+      }
+      else if (columnInFile == 1)
+      {
+        nodeY = compareStringToVector(line, nodes, n);
+        cout << " and the node it should connect to is: " << line << ". " << endl;
+        columnInFile = 2;
+      }
+      else if (columnInFile == 2)
+      {
+        graph1.addEdge(nodeX, nodeY); //revert these!!!
+        //cout << line << endl;
+        columnInFile = 0;
+        nodeX = 0;
+        nodeY = 0;
+      }
+    }
+    //fix this
+    graph1.printEulerTour(nodes);
+  }
+  else
+  {
+    cout << "File not found.";
+  }
 
-				nodeX = compareStringToVector(line, nodes, n);
-				cout << "Name of node is: " << line << ", ";
-				columnInFile = 1;
-			}
-			else if (columnInFile == 1)
-			{
-				nodeY = compareStringToVector(line, nodes, n);
-				cout << " and the node it should connect to is: " << line << ". " << endl;
-				columnInFile = 2;
-			}
-			else if (columnInFile == 2)
-			{
-				graph1.connectNodes(nodeX, nodeY);
-				//cout << line << endl;
-				columnInFile = 0;
-				nodeX = 0;
-				nodeY = 0;
-			}
-		}
-		graph1.test(graph1);
-	}
-	else
-	{
-		cout << "File not found.";
-	}
-
-	cout << "From vector: ";
-	for (int i = 0; i < n; i++)
-	{
-		cout << nodes[i] << " ";
-	}
-	list<int>::iterator l;
-	cout << endl
-		 << "From list: ";
-	for (l = nodesInt.begin(); l != nodesInt.end(); l++)
-	{
-		cout << *l << " ";
-	}
-	cout << "From adjacencylist: ";
-	myFile.close();
-	return 0;
+  cout << "From vector: ";
+  for (int i = 0; i < n; i++)
+  {
+    cout << nodes[i] << " ";
+  }
+  list<int>::iterator l;
+  cout << endl
+       << "From list: ";
+  for (l = nodesInt.begin(); l != nodesInt.end(); l++)
+  {
+    cout << *l << " ";
+  }
+  myFile.close();
+  return 0;
 }
 
 int compareStringToVector(string comparison, vector<string> vectorWithNodes, int n)
 {
-	int caseNr = -1;
-	for (int y = 0; y < n; y++)
-	{
-		if (vectorWithNodes[y] == comparison)
-		{
-			caseNr = y;
-		}
-	}
-	if (caseNr == -1)
-	{
-		cout << "Something went wrong chief... sorry boot dat." << endl;
-	}
-	return caseNr;
+  int caseNr = -1;
+  for (int y = 0; y < n; y++)
+  {
+    if (vectorWithNodes[y] == comparison)
+    {
+      caseNr = y;
+    }
+  }
+  if (caseNr == -1)
+  {
+    cout << "Something went wrong chief... sorry boot dat." << endl;
+  }
+  return caseNr;
 }
